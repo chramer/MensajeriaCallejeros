aba674daaca8a1513296476f7edcff3c7846ef2a
Nicolas Ovejero <chramersoft@outlook.com>
2018-12-07 00:27:17 -0300

    a

--- MensajeriaCallejeros/PagoCadetes.cs
+++ MensajeriaCallejeros/PagoCadetes.cs
@@ -307,7 +307,7 @@ namespace MensajeriaCallejeros
                 conexion.Open();
                 sentencia = "insert into TB_CADET_REG_PAGO (id_reg_pg,id_cadet,fecpago,fecven,fecreal,fecpro,importe,sdo_total,sdo_restante,tip_pago,compensado,comentario) " +
                     "values ('" + id_reg + "','" + id + "','"+ String.Format("{0:yyyy-MM-dd}", dateTimePicker1.Value) + "','" + String.Format("{0:yyyy-MM-dd}", dateTimePicker2.Value) + "'," +
-                    "'" + String.Format("{0:yyyy-MM-dd hh:mm:ss tt}", DateTime.Today) + "','" + String.Format("{0:yyyy-MM-dd}", DateTime.Today) + "','" + importe + "','" + importe_cadete + "','" + resto + "','" + tip_pago + "','" + compensa + "','" + textBox1.Text + "')";
+                    "'" + String.Format("{0:yyyy-MM-dd hh:mm:ss.tttt}", DateTime.Today) + "','" + String.Format("{0:yyyy-MM-dd}", DateTime.Today) + "','" + importe + "','" + importe_cadete + "','" + resto + "','" + tip_pago + "','" + compensa + "','" + textBox1.Text + "')";
                 FbCommand cmd = new FbCommand(sentencia, conexion);
                 cmd.ExecuteNonQuery();
                 cmd = null;
